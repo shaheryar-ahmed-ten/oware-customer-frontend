f18db67468e6ad2dea95fd24dd463f98ad947ad0
HunbalSiddiqui <hunbal.dha@gmail.com>
2021-05-28 07:25:56 -0700

    Code cleanup.

--- src/components/widgets/PrimaryWidget.js
+++ src/components/widgets/PrimaryWidget.js
@@ -1,4 +1,4 @@
-import { Card, Grid, makeStyles, Typography } from '@material-ui/core'
+import { Card, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
 
 const useStyles = makeStyles({
