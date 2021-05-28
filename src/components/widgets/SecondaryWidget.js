0f03447d2d2d2a8248e2927fbc2cba7c618d940a
HunbalSiddiqui <hunbal.dha@gmail.com>
2021-05-28 06:30:35 -0700

    Secondary widget style updated.

--- src/components/widgets/SecondaryWidget.js
+++ src/components/widgets/SecondaryWidget.js
@@ -3,21 +3,50 @@ import React from 'react'
 
 const useStyles = makeStyles({
     root: {
-        border: "1px dotted black",
-        height: 200,
+        height: 227,
         display: "flex",
         flexFlow: "column",
         justifyContent: "center",
         boxSizing: "border-box",
-        padding: "10px 10px"
+        padding: "19px 10px",
+        backgroundColor: "#FFFFFF",
+        borderRadius: "4px",
+        border: "1px solid #F0F0F0"
     },
     title: {
-        fontSize: 14,
+        fontSize: 18,
         display: "flex",
+        lineHeight: "22px",
+        alignItems: "center",
+        fontWeight: "600"
     },
     details: {
         display: "flex",
         justifyContent: "space-between"
+    },
+    detailsHead: {
+        fontWeight: 400,
+        fontSize: 16,
+        lineHeight: "19.36px"
+    },
+    detailsValue: {
+        fontWeight: 600,
+        fontSize: 16,
+        lineHeight: "19px",
+    },
+    value: {
+        fontWeight: 700,
+        fontSize: 50,
+        lineHeight: "61px",
+        display: "flex",
+        alignItems: "center"
+    },
+    type: {
+        fontWeight: 600,
+        fontSize: 16,
+        lineHeight: "19px",
+        boxSizing: "border-box",
+        paddingLeft: "10px"
     }
 })
 function SecondaryWidget({ icon, name, value, totalQuantity, totalVolume, type }) {
@@ -29,28 +58,29 @@ function SecondaryWidget({ icon, name, value, totalQuantity, totalVolume, type }
                     {icon}
                     {name}
                 </Typography>
-                <Typography variant="h1" component="h2">
+                <Typography className={classes.value}>
                     {value}
+                    <span className={classes.type}>
+                        {type}
+                    </span>
                 </Typography>
-                <Typography variant="h5" component="h2">
-                    {type}
-                </Typography>
+
                 <br />
                 <Divider />
                 <br />
                 <div className={classes.details}>
-                    <Typography variant="h5" component="h2" gutterBottom>
+                    <Typography className={classes.detailsHead} gutterBottom>
                         Total Quantity
                     </Typography>
-                    <Typography variant="h5" component="h2" gutterBottom>
+                    <Typography className={classes.detailsValue} gutterBottom>
                         {totalQuantity}Kgs
                     </Typography>
                 </div>
                 <div className={classes.details}>
-                    <Typography variant="h5" component="h2" gutterBottom>
+                    <Typography className={classes.detailsHead} gutterBottom>
                         Total Volume
                     </Typography>
-                    <Typography variant="h5" component="h2" gutterBottom>
+                    <Typography className={classes.detailsValue} gutterBottom>
                         {totalVolume}Cm3
                     </Typography>
                 </div>
