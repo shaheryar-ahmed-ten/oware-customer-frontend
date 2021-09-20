af07c912f0a1e358258ccfe3102f0d2901663064
muhammadhamza703 <muhammad.hamza@experia.ai>
2021-09-20 15:55:14 +0500

    CP-Logo-427

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -13,17 +13,18 @@ export default function Logo({ variant }) {
 
   const getRelations = e => {
     // e.preventDefault();
-    axios.get(getURL('/user/company'))
+    axios.get(getURL('user/company'))
     .then((res) => {
       
-      console.log(res.data);
+      // console.log(res.data);
       res.data.file && res.data.file.id ? setFileLocation(res.data.file.id): setFileLocation('');
 
-      console.log(getURL('/preview/',filelocation))
+      // console.log(getURL('/preview',filelocation))
   })
   }
-  const Logo = filelocation && getURL('/preview/',filelocation) ? getURL('/preview',filelocation) : noImageFound;
+  const Logo = filelocation && getURL('preview',filelocation) ? getURL('preview',filelocation) : noImageFound;
+  console.log("Logo",Logo)
   return (
-    <a href = "/dashboard" ><img src={Logo} alt="oware logo" /></a>
+    <a href = "dashboard" ><img src={Logo} alt="oware logo" /></a>
   );
 };
