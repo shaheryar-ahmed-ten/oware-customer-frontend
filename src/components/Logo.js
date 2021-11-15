de06a47e36d0eb3822a51f3fc747450c2e7a33ea
Muhammad Hamza <muhammad.hamza@experia.ai>
2021-11-15 18:30:15 +0500

    Height Width Fixation

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -23,7 +23,7 @@ export default function Logo({ variant }) {
   // const Logo = filelocation && getURL('preview',filelocation) ? getURL('preview',filelocation) : noImageFound;
   return (
     <a href="/dashboard">
-      <img src={Logo} alt="" height="37px" width="142px" />
+      <img src={Logo} alt="" height="40px" width="150px" />
     </a>
   );
 }
